93306361c8c9e6269a8d160bfa922fb70ad6b8c9
teste26academia <deciopadovanijr@gmail.com>
2026-06-02 12:48:12 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'kung-fu-garra-aguia-pg-v4';
+const ASSETS = ['./', './index.html', './app.js', './manifest.json', './assets/logo-oficial.png', './assets/icon-192.png', './assets/icon-512.png', './assets/apple-touch-icon.png', './assets/favicon-32.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
